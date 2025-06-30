a1faedf97e3f7b8ee4157ebf8f57c3cf08c43066
Fengzijie <1789136001@qq.com>
2025-06-30 16:51:22 +0800

    Initial commit

---
+++ local_configs/_base_/models/leformer.py
@@ -0,0 +1,43 @@
+# model settings
+# model settings
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained=None,
+    backbone=dict(
+        type='LEFormer',
+        in_channels=3,
+        embed_dims=32,
+        num_stages=4,
+
+        # √的层数
+        # num_layers=[2, 2, 3, 6],
+         
+        num_layers=[2, 2, 3, 6],
+        num_heads=[1, 2, 5, 6],
+
+        patch_sizes=[7, 3, 3, 3],
+        strides=[4, 2, 2, 2],
+        sr_ratios=[8, 4, 2, 1],
+        out_indices=(0, 1, 2, 3),
+        mlp_ratio=4,
+        drop_rate=0.0,
+        ffn_classes=3),
+    decode_head=dict(
+        type='SegformerHead',
+        in_channels=[32, 64, 160, 192],
+        in_index=[0, 1, 2, 3],
+        channels=192,
+        dropout_ratio=0.1,
+        num_classes=2,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+
+        # 原始损失函数
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)
+
+    ),
+
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
